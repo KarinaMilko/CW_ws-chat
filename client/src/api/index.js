364808dc59c3faf5bb93898f3661d221053f6f90
Karina <milkokarina13@gmail.com>
2024-11-03 17:10:11 +0200

    Add socket

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
+import { io } from "socket.io-client";
 
 const axiosOptions = {
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: "http://127.0.0.1:5000/api",
 };
 
 const apiInstance = axios.create(axiosOptions);
 
-export const getMessages = limit => apiInstance.get(`/messages?limit=${limit}`);
+export const getMessages = (limit) =>
+  apiInstance.get(`/messages?limit=${limit}`);
 
-export const createMessage = newMessage =>
-  apiInstance.post('/messages', newMessage);
+export const createMessage = (newMessage) =>
+  apiInstance.post("/messages", newMessage);
+
+// -------------------
+const socketClient = io("ws://localhost:5000"); // 'connection'
+// socketClient.emit('connection', socketClient)
+
+socketClient.on("HELLO_TO_ME", () => {
+  console.log("Hello in our chat");
+});
+
+socketClient.on("NEW_PARTICIPANT", () => {
+  console.log("New participant come in");
+});
+
+socketClient.on("CHAT_WILL_CLOSED", () => {
+  console.log("We will closed in a few minutes");
+});
+
+socketClient.emit("SEND_TO_SERVER", "Message");
--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("./app");
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
+
+// ----------------
+const cors = { origin: "*" };
+const wsServer = new Server(httpServer, { cors });
+
+wsServer.on("connection", (socket) => {
+  console.log("Connection established");
+
+  // надіслати новому під'єднаному сокету привітання
+  socket.emit("HELLO_TO_ME");
+
+  // сповіщення про те, що під'єднався новий учасник
+  socket.broadcast.emit("NEW_PARTICIPANT");
+
+  // сповістити всіх, що чат закінчує роботу
+  wsServer.emit("CHAT_WILL_CLOSED");
+
+  wsServer.on("SEND_TO_SERVER", (message) => {
+    console.log("From client:", message);
+  });
+});
+// --------------
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
