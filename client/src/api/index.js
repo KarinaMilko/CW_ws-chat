e75760c2a0a3572d0e8473287d3431532b7dc322
Karina <milkokarina13@gmail.com>
2024-11-03 17:40:22 +0200

    Add new message to server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -10,23 +10,36 @@ const apiInstance = axios.create(axiosOptions);
 export const getMessages = (limit) =>
   apiInstance.get(`/messages?limit=${limit}`);
 
-export const createMessage = (newMessage) =>
-  apiInstance.post("/messages", newMessage);
+const socketClient = io("ws://localhost:5000"); // 'connection'
+
+export const createMessage = (newMessage) => {
+  socketClient.emit("NEW_MESSAGE", newMessage);
+};
+
+socketClient.on("NEW_MESSAGE_SUCCESS", (payload) => {
+  console.log("payload :>> ", payload);
+});
+
+socketClient.on("NEW_MESSAGE_ERROR", (payload) => {
+  console.log("payload :>> ", payload);
+});
+
+//===========================================================================================================
+// export const createMessage = (newMessage) =>
+//   apiInstance.post("/messages", newMessage);
 
 // -------------------
-const socketClient = io("ws://localhost:5000"); // 'connection'
+// const socketClient = io("ws://localhost:5000"); // 'connection'
 // socketClient.emit('connection', socketClient)
 
-socketClient.on("HELLO_TO_ME", () => {
-  console.log("Hello in our chat");
-});
+// socketClient.on("HELLO_TO_ME", () => {
+//   console.log("Hello in our chat");
+// });
 
-socketClient.on("NEW_PARTICIPANT", () => {
-  console.log("New participant come in");
-});
+// socketClient.on("NEW_PARTICIPANT", () => {
+//   console.log("New participant come in");
+// });
 
-socketClient.on("CHAT_WILL_CLOSED", () => {
-  console.log("We will closed in a few minutes");
-});
-
-socketClient.emit("SEND_TO_SERVER", "Message");
+// socketClient.on("CHAT_WILL_CLOSED", () => {
+//   console.log("We will closed in a few minutes");
+// });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const { Server } = require("socket.io");
 const app = require("./app");
+const { Message } = require("./models");
 
 const PORT = process.env.PORT ?? 5000;
 
@@ -11,23 +12,43 @@ const cors = { origin: "*" };
 const wsServer = new Server(httpServer, { cors });
 
 wsServer.on("connection", (socket) => {
-  console.log("Connection established");
+  // console.log('Connection established');
 
-  // надіслати новому під'єднаному сокету привітання
-  socket.emit("HELLO_TO_ME");
-
-  // сповіщення про те, що під'єднався новий учасник
-  socket.broadcast.emit("NEW_PARTICIPANT");
-
-  // сповістити всіх, що чат закінчує роботу
-  wsServer.emit("CHAT_WILL_CLOSED");
-
-  wsServer.on("SEND_TO_SERVER", (message) => {
-    console.log("From client:", message);
+  socket.on("NEW_MESSAGE", async (payload) => {
+    try {
+      // закинути повідомлення в базу
+      const createdMessage = await Message.create(payload);
+      // успіх - відправити повідомлення всім
+      wsServer.emit("NEW_MESSAGE_SUCCESS", createdMessage);
+    } catch (error) {
+      // помилка - відправити помилку собі
+      socket.emit("NEW_MESSAGE_ERROR", error);
+    }
   });
 });
+
 // --------------
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
 });
+
+//==============================================
+
+// wsServer.on("connection", (socket) => {
+//   console.log("Connection established");
+
+//   // надіслати новому під'єднаному сокету привітання
+//   socket.emit("HELLO_TO_ME");
+
+//   // сповіщення про те, що під'єднався новий учасник
+//   socket.broadcast.emit("NEW_PARTICIPANT");
+
+//   // сповістити всіх, що чат закінчує роботу
+//   wsServer.emit("CHAT_WILL_CLOSED");
+
+//   wsServer.on("SEND_TO_SERVER", (message) => {
+//     console.log("From client:", message);
+//   });
+// });
+// --------------
