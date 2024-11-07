a42c85dbd89681d4718d801ea2f0052a104ad216
Karina <milkokarina13@gmail.com>
2024-11-07 21:21:27 +0200

    Refactor server^ split http&socket server

--- server/index.js
+++ server/index.js
@@ -1,31 +1,33 @@
 const http = require("http");
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 const app = require("./app");
-const { Message } = require("./models");
+// const { Message } = require("./models");
+const initSocket = require("./socket");
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
 
+initSocket(httpServer);
 // ----------------
-const cors = { origin: "*" };
-const wsServer = new Server(httpServer, { cors });
+// const cors = { origin: "*" };
+// const wsServer = new Server(httpServer, { cors });
 
-wsServer.on("connection", (socket) => {
-  console.log("Connection established");
+// wsServer.on("connection", (socket) => {
+//   console.log("Connection established");
 
-  socket.on("NEW_MESSAGE", async (payload) => {
-    try {
-      // закинути повідомлення в базу
-      const createdMessage = await Message.create(payload);
-      // успіх - відправити повідомлення всім
-      wsServer.emit("NEW_MESSAGE_SUCCESS", createdMessage);
-    } catch (error) {
-      // помилка - відправити помилку собі
-      socket.emit("NEW_MESSAGE_ERROR", error);
-    }
-  });
-});
+//   socket.on("NEW_MESSAGE", async (payload) => {
+//     try {
+//       // закинути повідомлення в базу
+//       const createdMessage = await Message.create(payload);
+//       // успіх - відправити повідомлення всім
+//       wsServer.emit("NEW_MESSAGE_SUCCESS", createdMessage);
+//     } catch (error) {
+//       // помилка - відправити помилку собі
+//       socket.emit("NEW_MESSAGE_ERROR", error);
+//     }
+//   });
+// });
 
 // --------------
 
---
+++ server/socket.js
@@ -0,0 +1,21 @@
+const { Server } = require("socket.io");
+const { Message } = require("./models");
+const cors = { origin: "*" };
+function initSocket(httpServer) {
+  const wsServer = new Server(httpServer, { cors });
+  wsServer.on("connection", (socket) => {
+    console.log("Connection established");
+    socket.on("NEW_MESSAGE", async (payload) => {
+      try {
+        // закинути повідомлення в базу
+        const createdMessage = await Message.create(payload);
+        // успіх - відправити повідомлення всім
+        wsServer.emit("NEW_MESSAGE_SUCCESS", createdMessage);
+      } catch (error) {
+        // помилка - відправити помилку собі
+        socket.emit("NEW_MESSAGE_ERROR", error);
+      }
+    });
+  });
+}
+module.exports = initSocket;
