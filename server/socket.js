bd09de2bf13ca443f27c7d7f81aae1686025d0b6
Karina <milkokarina13@gmail.com>
2024-11-07 21:35:38 +0200

    Move socket events to constants

--- server/socket.js
+++ server/socket.js
@@ -1,19 +1,24 @@
 const { Server } = require("socket.io");
 const { Message } = require("./models");
+const {
+  SOCKET_SERVER_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
+} = require("./constants");
+
 const cors = { origin: "*" };
 function initSocket(httpServer) {
   const wsServer = new Server(httpServer, { cors });
   wsServer.on("connection", (socket) => {
     console.log("Connection established");
-    socket.on("NEW_MESSAGE", async (payload) => {
+
+    socket.on(NEW_MESSAGE, async (payload) => {
       try {
         // закинути повідомлення в базу
         const createdMessage = await Message.create(payload);
         // успіх - відправити повідомлення всім
-        wsServer.emit("NEW_MESSAGE_SUCCESS", createdMessage);
+        wsServer.emit(NEW_MESSAGE_SUCCESS, createdMessage);
       } catch (error) {
         // помилка - відправити помилку собі
-        socket.emit("NEW_MESSAGE_ERROR", error);
+        socket.emit(NEW_MESSAGE_ERROR, error);
       }
     });
   });
