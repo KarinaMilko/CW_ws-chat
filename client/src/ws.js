4b62efead9ff0e844518ea30efab8057b8664fdd
Karina <milkokarina13@gmail.com>
2024-11-07 21:50:24 +0200

    Move socket events to constants on client

--- client/src/ws.js
+++ client/src/ws.js
@@ -3,17 +3,22 @@ import {
   newMessageError,
   newMessageSuccess,
 } from "../store/slices/messagesSlice";
-// -------------------
+
+import CONSTANTS from "../constants";
+const {
+  SOCKET_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
+} = CONSTANTS;
+
 const socketClient = io("ws://localhost:5000");
 // socketClient.emit('connection', socketClient)
 export const createMessage = (newMessage) => {
-  socketClient.emit("NEW_MESSAGE", newMessage);
+  socketClient.emit(NEW_MESSAGE, newMessage);
 };
 export const initSocket = (store) => {
-  socketClient.on("NEW_MESSAGE_SUCCESS", (payload) => {
+  socketClient.on(NEW_MESSAGE_SUCCESS, (payload) => {
     store.dispatch(newMessageSuccess(payload));
   });
-  socketClient.on("NEW_MESSAGE_ERROR", (payload) => {
+  socketClient.on(NEW_MESSAGE_ERROR, (payload) => {
     store.dispatch(newMessageError(payload));
   });
 };
